98e15e4583ad3e305e5ddce34d625c2930f43029
Flynn-Behnke <113999225+Flynn-Behnke@users.noreply.github.com>
2022-10-27 23:27:45 -0500

    Create PS5P2.py

---
+++ PS5P2.py
@@ -0,0 +1,7 @@
+start = float(input("Enter start value "))
+inc  = float(input("Enter increment value "))
+end = float(input("Enter end value "))
+count = start
+while count <= end:
+  print(count)
+  count = count + inc
